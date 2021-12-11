23e31c2ba72617a312da82831799b713ebb30f7f
tengine8000 <torty.emmanuel@gmail.com>
2021-12-11 09:36:29 +0100

    Initial commit

---
+++ src/main/java/com/emmanueltorty/walletapp/WalletappApplication.java
@@ -0,0 +1,13 @@
+package com.emmanueltorty.walletapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletappApplication.class, args);
+	}
+
+}
